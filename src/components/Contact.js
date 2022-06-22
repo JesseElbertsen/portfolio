93fc555bf1cc0b8890a1d6b0405cfae76b197d30
Juicy_Jesse <91599008+JesseElbertsen@users.noreply.github.com>
2022-06-22 11:03:38 +0200

    update contact.js
    updated contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ function Contact(){
     return(
         <section>
             <div className="flex flex-col items-center md:p-24 mt-6">
-                <div className="flex flex-col w-1/2">
+                <div className="flex flex-col p-5">
                     <h1 className="font-bold text-4xl pb-8">Contact</h1>
                     <h1 className="font-bold text-2xl pb-4">Voor eventuele samenwerking of om contact op te nemen kunt u mij mailen naar: </h1>
                     <a className="text-red-600 mb-5 text-xl" href="mailto:jesse.elbertsen@gmail.com">jesse.elbertsen@gmail.com.</a> 
